07643f50abb13220ab03dd5167598c395d847fae
Zachary Perkins <ezmac028@gmail.com>
2020-10-07 19:40:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ function updateObjectWithKeyAndValue(obj, key, value) {
 
   return Object.assign({}, obj, { [key]: value })
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value
+
+  return obj
+}
